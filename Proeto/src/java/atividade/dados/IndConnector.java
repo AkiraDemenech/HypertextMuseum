0578040f72c7fee85b2c22754edea0d7bcc83ea6
Guilherme Akira <guiakira.demenechmori@gmail.com>
2019-07-02 21:06:27 -0300

    528491
    ...

--- Proeto/src/java/atividade/dados/IndConnector.java
+++ Proeto/src/java/atividade/dados/IndConnector.java
@@ -11,9 +11,11 @@ public class IndConnector {
                    "jdbc:mysql://localhost:3306/vkhutemas",
                    "root", "senha");
         } catch (SQLException sql) {
-            throw new NullPointerException ("Não se assuste, só a conexão com o Banco de Dados que não foi bem sucedida mesmo....\\n\\n"
-                    + "ABAIXO, A MENSAGEM DE ERRO OFICIAL DO JAVA SQL DRIVER MANAGER PARA PROUN/PROETO:\\n" + sql);
-            // return null; 
+            throw new NullPointerException ("Falha na Conexão com Banco de Dados " + 
+											"[deixamos a mensagem de erro assustadora logo ao final dessa aqui mais fofinha]\\n" + 
+											"Calma, pode ser que funcione depois.... \\n\\n JAVA SQL DRIVER MANAGER OFICIALMENTE PARA PROUN/PROETO:\\n" + sql);
+					//"Não se assuste, só a conexão com o Banco de Dados que não foi bem sucedida mesmo");
+            //	return null; */
         }
     }
 }
